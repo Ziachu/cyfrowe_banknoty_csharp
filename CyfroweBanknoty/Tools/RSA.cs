398af9f77cfabba8528fff20578775c834a25b3d
Michał <ziachmichal@gmail.com>
2016-01-24 00:02:41 +0100

    Copy previous RSA implementation

--- CyfroweBanknoty/Tools/RSA.cs
+++ CyfroweBanknoty/Tools/RSA.cs
@@ -1,14 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace CyfroweBanknoty.Tools
 {
-    class RSA
+    public class RSA
     {
-        // implement RSA
-            // use some .NET implementation
+        private RSACryptoServiceProvider rsa;
+
+        public RSA()
+        {
+            rsa = new RSACryptoServiceProvider();
+        }
+
+        //metoda szyfrująca
+        public byte[] Encrypt(string plain_text)
+        {
+            byte[] encryption;
+
+            encryption = rsa.Encrypt(Helper.GetBytes(plain_text), false);
+            return encryption;
+        }
+
+        //metoda deszyfrująca
+        public byte[] Decrypt(byte[] cryptogram)
+        {
+            byte[] plain_text;
+
+            plain_text = rsa.Decrypt(cryptogram, false);
+            return plain_text;
+        }
+
+        //metoda zwracająca klucz publiczny
+        public string GetPublicKey()
+        {
+            return rsa.ToXmlString(false);
+        }
+
+        //metoda zwracająca klucz prywatny
+        public string GetPrivateKey()
+        {
+            return rsa.ToXmlString(true);
+        }
+
+        public void SetPublicKey(string rsa_params)
+        {
+            rsa.FromXmlString(rsa_params);
+        }
     }
 }
