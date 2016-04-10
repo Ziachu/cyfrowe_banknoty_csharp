5160988d02374a9ced752ef65926d9ece2dba76d
Damian Dudek <damdud@st.amu.edu.pl>
2016-04-10 23:19:12 +0200

    Some Vendor changes

--- CyfroweBanknoty/Users/Vendor.cs
+++ CyfroweBanknoty/Users/Vendor.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
+using CyfroweBanknoty.Tools;
 
 namespace CyfroweBanknoty.Users
 {
@@ -23,11 +24,39 @@ namespace CyfroweBanknoty.Users
         // step 14. If banknote is valid, Vendor sends it over to Bank among the partially revealed Alice ids.
         // [jump to Bank.cs][steps 15.-16.]
 
+        // --------
+        // PART IV
+        // --------
+        //step 17. BUYING - Vendor Generates 100 random bits
+        //Step 18. Vendors sends those bits to Alice 
+        //Step 19. Alice sends to Vendor 100 bit commitments - which side - it depends of 100 random bits from vendor(1 - left, 0 - right)
+
         public Tools.RSA rsa;
+        private Connection bank_connection;
+        private Connection alice_connection;
+
+
+
+
+
         //Sprzedawca pobiera z Banku klucz publiczny K=[E,N] do weryfikacji podpisu
         public void getSignatureFromBank()
         {
+            if (bank_connection.socket.Connected)
+            {
+                var public_key_in_bytes = bank_connection.Receive(1);
+                var public_key_in_xml = Helper.GetStringFromBytes(public_key_in_bytes);
 
+                //Console.WriteLine("\t[debug]: publick_key_in_xml: " + public_key_in_xml);
+
+                rsa.SetPublicKey(public_key_in_xml);
+
+                Console.WriteLine("[info]: I've got public key from Bank!");
+            }
+            else
+            {
+                Console.WriteLine("[fail] Establish connection with Bank first!");
+            }
         }
         //sprzedawca otrzymuje od Alice banknot w raz z podpisem[Mj, Sj] i oblicza S' = M^e(mod N) i sprawdza czy podpis zgadza sie z S od Alice
         public void getBanknote()
@@ -92,5 +121,35 @@ namespace CyfroweBanknoty.Users
                 //wysylamy
             }
         }
+
+        //step 17
+        public byte[] generate100RandomBits()
+        {
+
+            byte[] bits = new byte[100];
+            Random rand = new Random();
+
+            for(int i=0; i<100; i++)
+            {
+                bits[i] = Helper.GetBytesInteger(rand.Next())[i];
+            }
+
+            return bits;
+
+        }
+
+        //step 18
+        public void sendRandomBits(byte[] bits)
+        {
+            alice_connection.Send(1, bits);
+
+        }
+
+        //step 19
+        public void getCommitments()
+        {
+
+        }
+
     }
 }
